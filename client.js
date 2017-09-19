7e08a975b542014734817f7ae625b9374f972d1d
Rico Wang <ricowang@qnap.com>
2017-09-19 16:14:01 +0800

    some test code

---
+++ client.js
@@ -0,0 +1,63 @@
+const net = require('net');
+
+import Agent from './module/agent';
+import VirtualMachine from './module/device/vm';
+import HardDisk from './module/device/disk';
+import NetworkDevice from './module/device/net';
+import TapDevice from './module/device/net/TapDevice';
+
+
+let vm = new VirtualMachine('New VM', (data) => {console.log(data)});
+vm.setCPUCore(2);
+vm.setMemory(512);
+vm.addDevice(new HardDisk('./test.qcow2'));
+
+let netdev= new NetworkDevice(new TapDevice());
+vm.addDevice(netdev);
+
+const client = new Agent(vm);
+
+try {
+    vm.start();
+} catch(e) {
+    console.log(e);
+}
+
+
+let p1 = new Promise((resolve, reject) => {
+        setTimeout(function() {
+            resolve("Success");
+            vm.stop();
+        }, 6000000)
+    }
+)
+
+let id = 1234;
+let cb = () => {
+    /*
+    client.getAgentVersion().then((value) => {
+        console.log('value: ' + value);
+    }).catch((err) => {
+        console.log('error' + err);
+    });
+
+    client.getNetworkInterfaces().then((value) => {
+        console.log('value: ' + value);
+    }).catch((err) => {
+        console.log('error' + err);
+    });
+
+    client.getNetworkInterfacebyDeviceName('eth0')
+        .then((item) => {
+        console.log(item);
+    }).catch((err) => {
+    });
+    */
+    client.sendTask('/sbin/ifconfig')
+        .catch((err) => {
+            if(err.errno === 'EAGAIN')
+                console.log('Please try again');
+        });
+}
+
+setInterval(cb, 2000);
